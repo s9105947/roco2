341655c44db97fd52f1dfcd890e89a73e39c84bc
Hannes T <s9105947@users.noreply.github.com>
2021-04-20 14:12:14 +0200

    rm elab/cstates

--- src/configurations/highlow/experiment.cpp
+++ src/configurations/highlow/experiment.cpp
@@ -29,20 +29,13 @@ void run_experiments(roco2::chrono::time_point starting_point, bool eta_only)
     auto high_low_duration = std::chrono::milliseconds(100);
     roco2::kernels::high_low_bs high_low_kernel(high_low_duration, high_low_duration);
 
-    roco2::cpu::shell cstatectl("", "elab cstate enable", "elab cstate enable");
-
     roco2::memory::numa_bind_local nbl;
 
     // ------ EDIT GENERIC SETTINGS BELOW THIS LINE ------
 
-    auto experiment_duration = std::chrono::seconds(10);
+    auto experiment_duration = std::chrono::seconds(1);
 
-    auto on_list = block_pattern(1);
-
-    auto cstate_list =
-        std::vector<roco2::cpu::shell::setting_type>{ { 0, "elab cstate enable --only POLL" },
-                                                      { 1, "elab cstate enable C1" },
-                                                      { 2, "elab cstate enable C2" } };
+    auto on_list = block_pattern(4);
 
     // ------ EDIT GENERIC SETTINGS ABOVE THIS LINE ------
 
@@ -64,18 +57,10 @@ void run_experiments(roco2::chrono::time_point starting_point, bool eta_only)
         plan.push_back(roco2::task::experiment_task(exp, kernel, on));
     };
 
-    auto setting = [&](auto lambda) { plan.push_back(roco2::task::make_lambda_task(lambda)); };
-
     // ------ EDIT TASK PLAN BELOW THIS LINE ------
 
-    for (const auto& cstate_setting : cstate_list)
-    {
-        setting([&cstatectl, cstate_setting]() { cstatectl.change(cstate_setting); });
-
-        for (const auto& on : on_list)
-        {
-            experiment(high_low_kernel, on);
-        }
+    for (const auto& on : on_list) {
+        experiment(high_low_kernel, on);
     }
 
     // ------ EDIT TASK PLAN ABOVE THIS LINE ------
