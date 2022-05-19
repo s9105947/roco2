1fa509c16429bf00b0f42e7360d220b063c49cf7
Hannes T <s9105947@users.noreply.github.com>
2022-05-19 17:09:46 +0200

    high-low: add option to change workload type for high&low phases

--- include/roco2/kernels/high_low.hpp
+++ include/roco2/kernels/high_low.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_ROCO2_KERNELS_HIGH_LOW_HPP
 
 #include <chrono>
+#include <cstdint>
 
 #include <roco2/chrono/util.hpp>
 #include <roco2/kernels/base_kernel.hpp>
@@ -12,14 +13,32 @@ namespace roco2
 {
 namespace kernels
 {
+    /** encode a workload type
+     * Each workload is assigned one bit of a 64 bit int,
+     * s.t. one can store multiple workloads in the same metric.
+     *
+     * Note: use scoped enum to avoid cluttering namespace.
+     */
+    enum class workload_t : uint64_t {
+        stdlib_sleep = 1 << 0,
+        roco2_busy_wait = 1 << 1,
+        ppc64le_linux_hmt_verylow = 1 << 2,
+    };
+    
     class high_low_bs : public base_kernel
     {
     public:
         template <class DT_HIGH, class DT_LOW>
-        high_low_bs(DT_HIGH high_time, DT_LOW low_time)
-        : high_time_(std::chrono::duration_cast<roco2::chrono::time_point::duration>(high_time)),
+        high_low_bs(DT_HIGH high_time,
+                    DT_LOW low_time,
+                    workload_t high_workload = workload_t::roco2_busy_wait,
+                    workload_t low_workload = workload_t::stdlib_sleep)
+        : high_workload(high_workload),
+          low_workload(low_workload),
+          high_time_(std::chrono::duration_cast<roco2::chrono::time_point::duration>(high_time)),
           low_time_(std::chrono::duration_cast<roco2::chrono::time_point::duration>(low_time))
         {
+            // nop, only exists for initializer list
         }
 
         roco2::chrono::time_point::duration high_time() const {
@@ -39,7 +58,28 @@ namespace kernels
             return 12;
         }
 
+        /// workload to be executed during high phase, default: roco2_busy_wait
+        const workload_t high_workload;
+        /// workload to be executed during low phase, default stdlib_sleep
+        const workload_t low_workload;
+
     private:
+        /// run given workload until deadline
+        void run_workload_until(roco2::chrono::time_point deadline, const workload_t workload_to_run) {
+            switch(workload_to_run) {
+            case workload_t::roco2_busy_wait:
+                roco2::chrono::busy_wait_until(deadline);
+                break;
+
+            case workload_t::stdlib_sleep:
+                std::this_thread::sleep_until(deadline);
+                break;
+                
+            default:
+                throw std::logic_error("unknown workload: " + std::to_string(static_cast<uint64_t>(workload_to_run)));
+            }
+        }
+
         void run_kernel(roco2::chrono::time_point tp) override
         {
 #ifdef HAS_SCOREP
@@ -63,10 +103,10 @@ namespace kernels
                     deadline += high_time_;
                     if (deadline >= tp)
                     {
-                        roco2::chrono::busy_wait_until(tp);
+                        run_workload_until(tp, high_workload);
                         break;
                     }
-                    roco2::chrono::busy_wait_until(deadline);
+                    run_workload_until(deadline, high_workload);
                 }
 
                 {
@@ -76,10 +116,10 @@ namespace kernels
                     deadline += low_time_;
                     if (deadline >= tp)
                     {
-                        std::this_thread::sleep_until(tp);
+                        run_workload_until(tp, low_workload);
                         break;
                     }
-                    std::this_thread::sleep_until(deadline);
+                    run_workload_until(deadline, low_workload);
                 }
 
                 loops++;
